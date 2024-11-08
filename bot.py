02869e4f6a532bbee12abde29e2c329816664a29
Andrij-Ghorbunov <androniq@gmail.com>
2024-11-09 00:03:11 +0200

    Try

--- bot.py
+++ bot.py
@@ -7,11 +7,15 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 async def roll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Your message is kuku')
+    await update.message.reply_text(f'Your message is f{update.message.text}')
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Error')
 
 load_dotenv()
 app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
 
+app.add_error_handler(error)
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("roll", roll))
 

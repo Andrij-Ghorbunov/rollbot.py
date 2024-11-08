dc47b7e52ea418ba8ab49610faf51f52d84a2e4d
Andrij-Ghorbunov <androniq@gmail.com>
2024-11-08 22:39:27 +0200

    Test

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 async def roll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'I am deployed from git')
+    await update.message.reply_text(f'Your message is: ' + update.message)
 
 load_dotenv()
 app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
